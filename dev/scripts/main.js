c03c5b43b6f4fc58625147151e427a2b692ab996
Navdip-Kaur <navdipkaur88@gmail.com>
2016-10-25 12:48:09 -0400

    initial Commit

---
+++ dev/scripts/main.js
@@ -0,0 +1,6 @@
+const ohHey = "Hello World";
+
+// const number = [1,2,3,];
+// var number = Math.max(...number);
+
+console.log(ohHey);
---
+++ public/scripts/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var ohHey = "Hello World";
+
+// const number = [1,2,3,];
+// var number = Math.max(...number);
+
+console.log(ohHey);
